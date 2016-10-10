71ab79d4d216ea37c1a3c06ef2af049c54ff34b0
Jeff Layton <jlayton@redhat.com>
2016-10-10 08:13:36 -0400

    client: fix AT_SYMLINK_NOFOLLOW handling
    We're passing the reverse to path_walk. It should be true if we want
    to follow links.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -6656,7 +6656,7 @@ int Client::setattrx(const char *relpath, struct ceph_statx *stx, int mask,
 
   filepath path(relpath);
   InodeRef in;
-  int r = path_walk(path, &in, perms, flags & AT_SYMLINK_NOFOLLOW);
+  int r = path_walk(path, &in, perms, !(flags & AT_SYMLINK_NOFOLLOW));
   if (r < 0)
     return r;
   return _setattrx(in, stx, mask, perms);
@@ -6736,7 +6736,7 @@ int Client::statx(const char *relpath, struct ceph_statx *stx,
 
   unsigned mask = statx_to_mask(flags, want);
 
-  int r = path_walk(path, &in, perms, flags & AT_SYMLINK_NOFOLLOW, mask);
+  int r = path_walk(path, &in, perms, !(flags & AT_SYMLINK_NOFOLLOW), mask);
   if (r < 0)
     return r;
 
